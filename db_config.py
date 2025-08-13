fdfed328a2d407155da1b33181e6929052e46c26
Ranganath I B <25ranganath@gmail.com>
2025-08-13 12:53:29 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="ninu@180161",
+        password="PASSWORD",
         database="taskdb"
     )
     return connection
